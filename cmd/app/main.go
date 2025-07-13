cda5486ebb5cec4848da4bc007897ebdbdb0c2c0
Denis <netfixk@yandex.ru>
2025-07-13 19:20:37 +0300

    Build project structure/ Add main files, internal ang pkg folders

---
+++ cmd/app/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/wygnd/swim-help/internal/app"
+
+// Главный файл из которого будет собираться приложение
+func main() {
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wygnd/swim-help
+
+go 1.24.5
